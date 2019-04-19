5ee0bf36d7023ed6b25c73c8ebd835a0e46b5ad6
jackyicu <jackyicu@gmail.com>
2019-04-19 16:58:16 +0800

    add funtions to admin page

--- orderadmin/admin.py
+++ orderadmin/admin.py
@@ -10,6 +10,13 @@ from .models import Country, Client, Currency, Order, Category, Product, Cart, S
 
 # Register your models here.
 
+# class MyModelAdmin(admin.ModelAdmin):
+#     def get_queryset(self, request):
+#         qs = super(MyModelAdmin, self).get_queryset(request)
+#         if request.user.is_superuser:
+#             return qs
+#         return qs.filter(author=request.user)
+
 class CartInline(admin.TabularInline):
     model = Cart
     extra = 1
@@ -24,8 +31,8 @@ class PaymentInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     model = Order
-    list_display = ['date', 'sc_no', 'get_client_country', 'client','amount','currency']
-    list_filter = ('date',)
+    list_display = ['date', 'sales', 'sc_no', 'get_client_country', 'client','amount','currency', 'status']
+    list_filter = ('date','status','sales__name')
     list_per_page = 20
     search_fields = ['sc_no', 'client__name']
 
@@ -37,7 +44,7 @@ class OrderAdmin(admin.ModelAdmin):
     # fieldsets = (
     #     (None,('fields':())),
     # )
-    fields = (('date', 'client'),('pi_no','po_no', ),('sc_no', 'amount'), 'currency')
+    fields = (('date', 'client'),('pi_no','po_no', ),('sc_no', 'amount'), ('currency','status'),'remarks')
     inlines =[CartInline, ShipmentInline, PaymentInline]
     raw_id_fields = ('client',)
 
@@ -56,7 +63,7 @@ class ProductAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['date', 'get_sc_no', 'amount', 'currency', 'term']
+    list_display = ['date', 'get_sales','get_sc_no', 'get_client','amount', 'currency', 'term']
     list_filter = ('date',)
     list_per_page = 20
     search_fields = ['order__sc_no']
@@ -66,8 +73,55 @@ class PaymentAdmin(admin.ModelAdmin):
     # get_client_name.admin_order_field = 'client'
     get_sc_no.short_description = 'Contract_No'
 
+    def get_client(self, object):
+        return object.order.client.name
+    # get_client_name.admin_order_field = 'client'
+    get_client.short_description = 'Client'
+
+    def get_sales(self, object):
+        return object.order.sales.name
+    get_sales.admin_order_field = 'sales'
+    get_sales.short_description = 'Client'    
+
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(PaymentTerm)
+
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ['bl_date', 'get_sales','get_sc_no', 'carrier','tracking_no', 'bill_no', 'term']
+    list_filter = ('bl_date',)
+    list_per_page = 20
+    search_fields = ['order__sc_no']
+
+    def get_sc_no(self, object):
+        return object.order.sc_no
+    # get_client_name.admin_order_field = 'client'
+    get_sc_no.short_description = 'Contract_No'
+
+    def get_sales(self, object):
+        return object.order.sales.name
+    get_sales.admin_order_field = 'sales'
+    get_sales.short_description = 'Client'     
+
+admin.site.register(Shipment, ShipmentAdmin)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['get_order_date', 'serial_no','get_sc_no']
+    list_filter = ('bl_date',)
+    list_per_page = 20
+    search_fields = ['order__sc_no', 'serial_no']
+
+    def get_order_date(self, object):
+        return object.order.date
+    # get_client_name.admin_order_field = 'client'
+    get_order_date.short_description = 'Order Date'
+
+    def get_sc_no(self, object):
+        return object.order.sc_no
+    # get_client_name.admin_order_field = 'client'
+    get_sc_no.short_description = 'Contract_No'    
+
+admin.site.register(Cart, CartAdmin)
+
 admin.site.register(Currency)
 admin.site.register(Category)
 admin.site.register(Country)
--- orderadmin/models.py
+++ orderadmin/models.py
@@ -29,14 +29,23 @@ class Currency(models.Model):
     def __str__(self):
         return self.name
 
+class Sales(models.Model):
+    name = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.name    
+
 class Order(models.Model):
     date = models.DateField(verbose_name='Order Date')
+    sales = models.ForeignKey(Sales, on_delete=models.CASCADE)
     pi_no = models.CharField(max_length=15)
     po_no = models.CharField(max_length=15, blank=True)
     sc_no = models.CharField(max_length=15)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.ForeignKey(Currency,on_delete=None)
+    status = models.CharField(max_length=10, blank=True)
+    remarks = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.sc_no
@@ -58,11 +67,11 @@ class Cart(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     erp_code = models.CharField(max_length=9, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2,blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
     serial_no = models.CharField(max_length=8, blank=True)
     location = models.CharField(max_length=30, blank=True)
     installation_date = models.DateField(blank=True, null=True)
-    # price = models.DecimalField(max_digits=8, decimal_places=2)
-    # quantity = models.IntegerField()
 
     def __str__(self):
         return self.serial_no    
